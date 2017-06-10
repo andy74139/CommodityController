638c8f146cf4f12a20e36f3d89e8d98a525a269a
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-10 23:52:09 +0800

    Add CommodityController empty methods & add tests

---
+++ CommodityController/Commodity.cs
@@ -0,0 +1,22 @@
+﻿namespace CommodityController
+{
+    public class Commodity
+    {
+        public int Id;
+        public int Cost;
+        public int Revenue;
+        public int SellPrice;
+
+        public Commodity()
+        {
+        }
+
+        public Commodity(int id, int cost, int revenue, int sellPrice)
+        {
+            Id = id;
+            Cost = cost;
+            Revenue = revenue;
+            SellPrice = sellPrice;
+        }
+    }
+}
---
+++ CommodityControllerTests/CommodityControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace CommodityController.Tests
+{
+    [TestFixture]
+    public class CommodityControllerTests
+    {
+        private readonly List<Commodity> _TestCommodities = new List<Commodity>()
+        {
+            new Commodity(1, 1, 11, 21),
+            new Commodity(2, 2, 12, 22),
+            new Commodity(3, 3, 13, 23),
+            new Commodity(4, 4, 14, 24),
+            new Commodity(5, 5, 15, 25),
+            new Commodity(6, 6, 16, 26),
+            new Commodity(7, 7, 17, 27),
+            new Commodity(8, 8, 18, 28),
+            new Commodity(9, 9, 19, 29),
+            new Commodity(10, 10, 20, 30),
+            new Commodity(11, 11, 21, 31), 
+        };
+
+        [TestCase(3,      "Cost", ExpectedResult = new[] {  6, 15, 24, 21 }, TestName = "ThreeInGroup_GetCostSum_ShouldEqual")]
+        [TestCase(3,   "Revenue", ExpectedResult = new[] { 36, 45, 54, 41 }, TestName = "ThreeInGroup_GetRevenueSum_ShouldEqual")]
+        [TestCase(3, "SellPrice", ExpectedResult = new[] { 66, 75, 84, 61 }, TestName = "ThreeInGroup_GetRevenueSum_ShouldEqual")]
+        [TestCase(4,      "Cost", ExpectedResult = new[] { 10,  26, 30 }, TestName = "FourInGroup_GetCostSum_ShouldEqual")]
+        [TestCase(4,   "Revenue", ExpectedResult = new[] { 50,  66, 60 }, TestName = "FourInGroup_GetRevenueSum_ShouldEqual")]
+        [TestCase(4, "SellPrice", ExpectedResult = new[] { 90, 106, 90 }, TestName = "FourInGroup_GetRevenueSum_ShouldEqual")]
+        [TestCase(1,  "Cost", ExpectedResult = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, TestName = "OneInGroup_GetCostSum_ShouldListAllCosts")]
+        [TestCase(11, "Cost", ExpectedResult = new[] { 66 }, TestName = "ElevenInGroup_GetCostSum_ShouldHaveOneSumOfCosts")]
+        [TestCase(12, "Cost", ExpectedResult = new[] { 66 }, TestName = "TwelveInGroup_GetCostSum_ShouldHaveOneSumOfCosts")]
+        [TestCase(0,  "Cost", ExpectedResult = new[] { 0 },  TestName = "ZeroInGroup_GetCostSum_ShouldEqualZero")]
+        public int[] GetGroupValueSumTest_ShouldEqual(int amountPerGroup, string columnName)
+        {
+            //Arrange
+            var target = new CommodityController(_TestCommodities);
+
+            //Act
+            var actual = target.GetGroupValueSum(amountPerGroup, columnName);
+
+            //Assert
+            return actual;
+        }
+
+        [TestCase(-1, "Cost", TestName = "NegativeNumberInGroup_GetCostSum_ShouldThrowArgumentException")]
+        [TestCase(3, "WrongColumn", TestName = "PositiveNumberInGroup_GetWrongColumnSum_ShouldThrowArgumentException")]
+        public void GetGroupValueSumTest_ShouldThrowException(int amountPerGroup, string columnName)
+        {
+            //Arrange
+            var target = new CommodityController(_TestCommodities);
+
+            //Act & Assert
+            TestDelegate action = () => target.GetGroupValueSum(amountPerGroup, columnName);
+            Assert.Throws<ArgumentException>(action);
+        }
+    }
+}
